1afa5e85a0e7ffa7b1aebf4a23aac65203cafeba
Tebogo Mofokeng <tebohogivenmofokeng@gmail.com>
2025-01-16 11:09:09 +0200

    waste sort

--- tebogomofokeng/src/main/java/com/enviro/assessment/grad001/tebogomofokeng/repository/DisposalGuidelinesRepository.java
+++ tebogomofokeng/src/main/java/com/enviro/assessment/grad001/tebogomofokeng/repository/DisposalGuidelinesRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface DisposalGuidelinesRepository extends JpaRepository<DisposalGuidelines, Long> {
-    List<DisposalGuidelines> findByCategoryId(Long categoryId);
+
 }
--- tebogomofokeng/src/main/java/com/enviro/assessment/grad001/tebogomofokeng/repository/RecyclingTipsRepository.java
+++ tebogomofokeng/src/main/java/com/enviro/assessment/grad001/tebogomofokeng/repository/RecyclingTipsRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RecyclingTipsRepository extends JpaRepository<RecyclingTips, Long> {
-    List<RecyclingTips> findByCategoryId(Long categoryId);
+
 }
